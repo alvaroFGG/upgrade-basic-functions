7c2d27e991744621de927a20cad42a938ca24314
alvaroFGG <alvaro@mbp-de-alvaro.home>
2022-04-30 12:20:57 +0200

    cuarta iteracion

---
+++ iteracion4.js
@@ -0,0 +1,15 @@
+const numbers = [12, 21, 38, 5, 45, 37, 6];
+function average(array) {
+    let sum = 0;
+    let cont = 0;
+    console.log(array.length);
+    for(let i of array){
+        sum += i;
+        cont++;
+    }
+
+    return sum/cont;
+  
+}
+
+console.log("La media es de: ",average(numbers));
